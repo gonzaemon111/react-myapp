6d3505c68c1c74e6da6675483f0abf8345343d2f
gonzaemon111 <goki14914@icloud.com>
2019-09-20 15:27:44 +0900

    add src/components/App.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Form from './Form';
+import List from  './List';
+
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      todo: []
+    };
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  // データ保存
+  handleAdd(e){
+    console.log(e.target.title.value);
+    e.preventDefault(); //これを書かないとリダイレクトされてしまう
+  }
+  render() {
+    return (
+      <div className="siimple-box siimple--bg-dark">
+        <h1 className="siimple-box-title siimple--color-white">React Todo App</h1>
+        <Form handleAdd={this.handleAdd}/>
+        <div className="siimple-rule"></div>
+        <List/>
+      </div>
+    );
+  }
+}
